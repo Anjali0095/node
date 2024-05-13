ac0739df53244c85ddc500f67f85e373f39467ae
Anjali <smoni6465@gmail.com>
2024-05-13 19:15:48 +0530

    intialize

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({ message: "Server is working Great! :)" });
+  res.json({ message: "Server is working good woooooofffff! :)" });
 });
 
 app.listen(3700, () => {
-  console.log("Express is Connected on port 3700");
+  console.log("Express is Connected on port 3000");
 });
